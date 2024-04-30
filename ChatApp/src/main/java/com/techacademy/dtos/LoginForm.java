8dff2bbc855431994da6640f323f9d092a19d437
dir-FY24kajiwarashimpei <shinpei.kajiwara.h902@dirbato.co.jp>
2024-04-30 12:11:09 +0900

    PostgleSQLコネクト

---
+++ ChatApp/src/main/java/com/techacademy/dtos/LoginForm.java
@@ -0,0 +1,18 @@
+package com.techacademy.dtos;
+
+import lombok.Data;
+
+@Data
+public class LoginForm {
+    private String mailAddress;
+    private String password;
+    
+    public LoginForm() {
+        super();
+    }
+    
+    public LoginForm(String mailAddress, String password) {
+        this.mailAddress = mailAddress;
+        this.password = password;
+    }
+}
